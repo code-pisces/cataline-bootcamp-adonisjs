07bf60d60ed78eeb096bc16b68b568e204a7c24e
code-pisces <guh.contato.work@outlook.com.br>
2021-12-20 12:02:52 -0300

    Operaçoes de CRUD

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -1,10 +1,38 @@
-// import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
-// import PostModel
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Post from 'App/Models/Post'
+// import Database from '@ioc:Adonis/Core/Database'
+
 export default class PostsController {
-  index() {
-    // return PostModel.all
-    return ['post1', 'post2', 'post3']
+  public async index({}: HttpContextContract) {
+    const posts = await Post.all()
+
+    return posts
+  }
+
+  public async store({ request }: HttpContextContract) {
+    const data = request.only(['title', 'content'])
+    const post = Post.create(data)
+
+    return post
+  }
+
+  public async show({ params }: HttpContextContract) {
+    // const post = await Database.rawQuery(`select * from post where id = ${params.id}`) SQL PURO
+    const post = await Post.findOrFail(params.id)
+    return post
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    const data = request.only(['title', 'content'])
+    post.merge(data)
+    await post.save()
+
+    return post
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const post = await Post.findOrFail(params.id)
+    await post.delete()
   }
 }
-
-// Route -> Controller -> Models
